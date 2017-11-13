b92d7d20571379ff2dbf2bdd2a9313aec592b0f9
Cristobal Valenzuela <cvalenzuela@nyu.edu>
2017-11-13 13:14:35 -0500

    fix :

--- utils/paths.js
+++ utils/paths.js
@@ -2,14 +2,14 @@
 Handy Paths Module
 */
 
-const IP = '165.227.188.111';
-const PORT = ':9888'
 const PROTOCOL = 'http://'
+const IP = '165.227.188.111';
+const PORT = '9888'
 
 module.exports = {
   API: '/api',
-  UPLOADS: PROTOCOL + IP + PORT + '/uploads/',
-  DELETEMEDIA: PROTOCOL + IP + PORT + '/api/deleteMedia/',
+  UPLOADS: PROTOCOL + IP + ':' + PORT + '/uploads/',
+  DELETEMEDIA: PROTOCOL + IP + ':' + PORT + '/api/deleteMedia/',
   PORT,
   IP
 }
